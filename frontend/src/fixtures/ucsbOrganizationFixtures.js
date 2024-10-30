3cc2d0f82ee4e2aea163766494603d14d1010cbf
Kevin Yan <kevinyan@169-231-31-55.wireless.ucsb.edu>
2024-10-30 16:40:17 -0700

    fixed formatting

--- frontend/src/fixtures/ucsbOrganizationFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -1,31 +1,30 @@
 const ucsbOrganizationFixtures = {
-    oneOrg: {
-        orgCode: "BAL",
-        orgTranslationShort: "Basketball Club",
-        orgTranslation: "UCSB Basketball Club",
-        inactive: false,
-    },
-    threeOrgs: [
+  oneOrg: {
+    orgCode: "BAL",
+    orgTranslationShort: "Basketball Club",
+    orgTranslation: "UCSB Basketball Club",
+    inactive: false,
+  },
+  threeOrgs: [
     {
-        orgCode: "BAL",
-        orgTranslationShort: "Basketball Club",
-        orgTranslation: "UCSB Basketball Club",
-        inactive: false,
-   },
-   {
-        orgCode: "SOC",
-        orgTranslationShort: "Soccer Club",
-        orgTranslation: "UCSB Soccer Club",
-        inactive: false,
+      orgCode: "BAL",
+      orgTranslationShort: "Basketball Club",
+      orgTranslation: "UCSB Basketball Club",
+      inactive: false,
     },
     {
-        orgCode: "CHE",
-        orgTranslationShort: "Chess Club",
-        orgTranslation: "UCSB Chess Club",
-        inactive: false,
+      orgCode: "SOC",
+      orgTranslationShort: "Soccer Club",
+      orgTranslation: "UCSB Soccer Club",
+      inactive: false,
     },
-    ],
-  };
-  
-  export { ucsbOrganizationFixtures };
-  
+    {
+      orgCode: "CHE",
+      orgTranslationShort: "Chess Club",
+      orgTranslation: "UCSB Chess Club",
+      inactive: false,
+    },
+  ],
+};
+
+export { ucsbOrganizationFixtures };
