c5b4d7c09485f596b39612aed8bbaee4f1e41ba6
Kevin Yan <kevinyan@169-231-31-55.wireless.ucsb.edu>
2024-10-30 16:32:54 -0700

    added fixtures json file

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,31 @@
+const ucsbOrganizationFixtures = {
+    oneOrg: {
+        orgCode: "BAL",
+        orgTranslationShort: "Basketball Club",
+        orgTranslation: "UCSB Basketball Club",
+        inactive: false,
+    },
+    threeOrgs: [
+    {
+        orgCode: "BAL",
+        orgTranslationShort: "Basketball Club",
+        orgTranslation: "UCSB Basketball Club",
+        inactive: false,
+   },
+   {
+        orgCode: "SOC",
+        orgTranslationShort: "Soccer Club",
+        orgTranslation: "UCSB Soccer Club",
+        inactive: false,
+    },
+    {
+        orgCode: "CHE",
+        orgTranslationShort: "Chess Club",
+        orgTranslation: "UCSB Chess Club",
+        inactive: false,
+    },
+    ],
+  };
+  
+  export { ucsbOrganizationFixtures };
+  
