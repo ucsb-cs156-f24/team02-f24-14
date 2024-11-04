fe643971f23df030463d340699598107c1532631
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2024-11-04 14:25:19 -0800

    removed unused regex

--- frontend/src/main/components/RecommendationRequests/RecommendationRequestForm.js
+++ frontend/src/main/components/RecommendationRequests/RecommendationRequestForm.js
@@ -26,9 +26,6 @@ function RecommendationRequestForm({
   const email_regex = /\S+@\S+\.\S+/;
   // Stryker restore Regex
 
-  // Stryker disable next-line all
-  const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
-
   return (
     <Form onSubmit={handleSubmit(submitAction)}>
       <Row>
