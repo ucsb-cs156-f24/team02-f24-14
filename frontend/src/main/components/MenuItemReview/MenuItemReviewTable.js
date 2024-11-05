55d67f1fe662f9415b1cd22536e3b8a2ff09cd4c
Riona Pampati <riona@ucsb.edu>
2024-11-05 00:16:15 -0800

    fixed utils tests

--- frontend/src/main/components/MenuItemReview/MenuItemReviewTable.js
+++ frontend/src/main/components/MenuItemReview/MenuItemReviewTable.js
@@ -25,7 +25,7 @@ export default function MenuItemReviewsTable({ reviews, currentUser }) {
   );
   // Stryker restore all
 
-  // Stryker disable next-line all : TODO try to make a good test for this
+  // Stryker disable next-line all
   const deleteCallback = async (cell) => {
     deleteMutation.mutate(cell);
   };
