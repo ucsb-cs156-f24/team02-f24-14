f25e5374fafe85fda430b41c44882f60daa1afe2
Kevin Yan <kevinyan@ucsb.edu>
2024-11-06 17:36:18 -0800

    might've fixed navbar

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -63,12 +63,6 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/restaurants">
                     Restaurants
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/articles">
-                    Articles
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/menuitemreviews">
-                    MenuItemReview
-                  </Nav.Link>
                   <Nav.Link as={Link} to="/ucsborganizations">
                     UCSB Organizations
                   </Nav.Link>
@@ -81,11 +75,14 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/ucsbdates">
                     UCSB Dates
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/recommendationrequests">
+                    Recommendation Requests
+                  </Nav.Link>
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/ucsborganization">
-                    UCSBOrganization
+                  <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitem">
+                    UCSBDiningCommonsMenuItem
                   </Nav.Link>
                 </>
               ) : (
