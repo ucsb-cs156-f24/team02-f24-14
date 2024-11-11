4235e11379503c3354104478c3ddb13e90f9cb66
Jennifer Zhu <JenniferZhu@ucsb.edu>
2024-11-10 18:03:20 -0800

    fix errors

---
+++ frontend/src/stories/pages/UCSBOrganization/UCSBOrganiztionEditPage.stories.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { ucsbOrganizationFixtures } from "fixtures/ucsbOrganizationFixtures";
+import { http, HttpResponse } from "msw";
+
+import UCSBOrganizationEditPage from "main/pages/UCSBOrganization/UCSBOrganizationEditPage";
+
+export default {
+  title: "pages/UCSBOrganization/UCSBOrganizationEditPage",
+  component: UCSBOrganizationEditPage,
+};
+
+const Template = () => <UCSBOrganizationEditPage storybook={true} />;
+
+export const Default = Template.bind({});
+Default.parameters = {
+  msw: [
+    http.get("/api/currentUser", () => {
+      return HttpResponse.json(apiCurrentUserFixtures.userOnly, {
+        status: 200,
+      });
+    }),
+    http.get("/api/systemInfo", () => {
+      return HttpResponse.json(systemInfoFixtures.showingNeither, {
+        status: 200,
+      });
+    }),
+    http.get("/api/ucsbOrganization", () => {
+      return HttpResponse.json(
+        ucsbOrganizationFixtures.threeOrgs,
+        {
+          status: 200,
+        },
+      );
+    }),
+    http.put("/api/ucsbOrganization", () => {
+      return HttpResponse.json({}, { status: 200 });
+    }),
+  ],
+};
